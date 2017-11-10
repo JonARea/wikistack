27f34d5bf860daef15911c643c96feef3246e600
Vesna Tan <tanvezi@gmail.com>
2017-11-10 17:00:16 -0500

    Linked database

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const nunjucks = require('nunjucks')
+const models = require('./models')
 
 app.use(morgan('dev'))
+
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+    noCache: true
+});
+
+models.Page.sync();
+models.User.sync();
+
+
 app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
 app.use(express.static('/public'))
